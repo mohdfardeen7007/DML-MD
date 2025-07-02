4913019b3f5c3cad663a4564b2d184e9fdaf7830
mohdfardeen7007 <162273323+mohdfardeen7007@users.noreply.github.com>
2025-07-02 22:14:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -31,9 +31,9 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/vcdwmp.j
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "DML-MD",
+BOT_NAME: process.env.BOT_NAME || "𝐅𝖾ᥣ𝗂𝗑",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "DML-MD",
+STICKER_NAME: process.env.STICKER_NAME || "𝐅𝖾ᥣ𝗂𝗑",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -41,15 +41,15 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255713XXXX",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "918127875972",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "DML-TECH",
+OWNER_NAME: process.env.OWNER_NAME || "𝐅𝖾ᥣ𝗂𝗑",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© POWERED BY DML-TECH*",
+DESCRIPTION: process.env.DESCRIPTION || "*© 𝐅𝖾ᥣ𝗂𝗑*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/vcdwmp.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *DML-MD*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *𝐅𝖾ᥣ𝗂𝗑*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -73,7 +73,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "255622220680",
+DEV: process.env.DEV || "918127875972",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
